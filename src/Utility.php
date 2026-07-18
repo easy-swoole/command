661819d4f72fb800c971b74c5dd38fdca45c22da
如果的如果 <admin@fosuss.com>
2026-07-18 10:01:56 +0800

    2.x重构

--- src/Utility.php
+++ src/Utility.php
@@ -31,4 +31,25 @@ class Utility
         }
         return $commandLine;
     }
+
+    public static function matchAlternativeCommands(string $input,array $commandNames):array
+    {
+        $alternatives = [];
+        foreach ($commandNames as $commandName) {
+            $lev = levenshtein($input, $commandName);
+            if ($lev <= strlen($input) / 3 || str_contains($commandName, $input)) {
+                $alternatives[$commandName] = $lev;
+            }
+        }
+        $threshold    = 1e3;
+        $alternatives = array_filter($alternatives, function ($lev) use ($threshold) {
+            return $lev < 2 * $threshold;
+        });
+        ksort($alternatives);
+
+        if ($alternatives) {
+            return array_keys($alternatives);
+        }
+        return [];
+    }
 }
